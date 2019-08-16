ee594e8c5209c757a60016dc42e59324aa424cae
anymyna <anymyna@126.com>
2019-08-16 23:45:15 +0800

    butterknife  add

--- app/src/main/java/com/example/ui/OkhttpDemoActivity.java
+++ app/src/main/java/com/example/ui/OkhttpDemoActivity.java
@@ -1,14 +1,18 @@
 package com.example.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -20,16 +24,36 @@ import okhttp3.Response;
 public class OkhttpDemoActivity extends AppCompatActivity {
     final OkHttpClient client = new OkHttpClient();
     String Tag = "OkhttpDemoActivity";
+
+    @Bind(R.id.btn_get)
+    Button btnGet;
+    @Bind(R.id.btn_post)
+    Button btnPost;
+    @Bind(R.id.layout)
+    RelativeLayout layout;
+
+
+    @OnClick({R.id.btn_get, R.id.btn_post})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_get:
+                break;
+            case R.id.btn_post:
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_okhttp_demo);
+        ButterKnife.bind(this);
     }
 
 
-    public void onClickPost(View v){
+    public void onClickPost(View v) {
 
-        Log.i(Tag,"onClickPost");
+        Log.i(Tag, "onClickPost");
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(10, TimeUnit.SECONDS)
@@ -43,7 +67,7 @@ public class OkhttpDemoActivity extends AppCompatActivity {
                 .post(formBody)
                 .url("http://httpbin.org/post")
                 .build();
-        //调用okHttpClient对象实现CallBack方法
+
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
@@ -54,18 +78,18 @@ public class OkhttpDemoActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-                Log.i(Tag,"response：" + response.body().string());
+                Log.i(Tag, "response：" + response.body().string());
 
             }
         });
 
     }
 
-    public void onClickGet(View v){
-        Log.i(Tag,"onClickGet");
+    public void onClickGet(View v) {
+        Log.i(Tag, "onClickGet");
 
 
-        final Request request=new Request.Builder()
+        final Request request = new Request.Builder()
                 .get()
                 .tag(this)
                 .url("https://api.github.com/events")
@@ -78,7 +102,7 @@ public class OkhttpDemoActivity extends AppCompatActivity {
                 try {
                     response = client.newCall(request).execute();
                     if (response.isSuccessful()) {
-                        Log.i(Tag,"response：" + response.body().string());
+                        Log.i(Tag, "response：" + response.body().string());
                     } else {
                         throw new IOException("Unexpected code " + response);
                     }
@@ -90,5 +114,6 @@ public class OkhttpDemoActivity extends AppCompatActivity {
 
 
     }
+
 
 }
