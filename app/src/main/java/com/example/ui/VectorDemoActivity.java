bfbceaa8819fd9d5edffc7968643c2727472c102
anymyna <anymyna@126.com>
2019-09-06 22:55:17 +0800

    增加防止系统截屏示例

--- app/src/main/java/com/example/ui/VectorDemoActivity.java
+++ app/src/main/java/com/example/ui/VectorDemoActivity.java
@@ -2,6 +2,7 @@ package com.example.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 
 public class VectorDemoActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class VectorDemoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_vector_demo);
     }
 
